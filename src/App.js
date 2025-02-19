bcdac0ee675868a5da9aa027d0a5397073a357a1
jonas pauli <jonaspauli089@gmail.com>
2025-02-19 19:18:43 +0100

    improve scaling

--- src/App.js
+++ src/App.js
@@ -8,26 +8,34 @@ import Projects from './routes/Projects';
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#012B3A] p-4 shadow-md flex justify-between items-center">
+    <nav className="bg-[#012B3A] p-3 shadow-md flex justify-between items-center">
       <div>
         {/* Desktop Logo */}
         <img
           src="/logo02.png"
           alt="Logo"
-          className="hidden md:block w-30 h-12 rounded-full"
+          className="hidden md:block w-30 h-10 rounded-full"
         />
         {/* Mobile Logo */}
         <img
           src="/logo192.png"
           alt="Mobile Logo"
-          className="block md:hidden w-30 h-12 rounded-full"
+          className="block md:hidden w-10 h-10 rounded-full"
         />
       </div>
-      <div>
-        <Link to="/" className="text-white hover:text-[#41EAD4] px-4">Home</Link>
-        <Link to="/team" className="text-white hover:text-[#41EAD4] px-4">Team</Link>
-        <Link to="/clients" className="text-white hover:text-[#41EAD4] px-4">Clients</Link>
-        <Link to="/projects" className="text-white hover:text-[#41EAD4] px-4">Projects</Link>
+      <div className="flex space-x-2 md:space-x-4">
+        <Link to="/" className="text-white hover:text-[#41EAD4] text-sm md:text-base px-2 md:px-4">
+          Home
+        </Link>
+        <Link to="/team" className="text-white hover:text-[#41EAD4] text-sm md:text-base px-2 md:px-4">
+          Team
+        </Link>
+        <Link to="/clients" className="text-white hover:text-[#41EAD4] text-sm md:text-base px-2 md:px-4">
+          Clients
+        </Link>
+        <Link to="/projects" className="text-white hover:text-[#41EAD4] text-sm md:text-base px-2 md:px-4">
+          Projects
+        </Link>
       </div>
     </nav>
   );
