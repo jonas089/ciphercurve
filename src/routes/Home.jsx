898a23d3e9dd326c97c12bbe49b45b3c219c6e62
jonas pauli <jonaspauli089@gmail.com>
2025-03-17 23:40:23 +0100

    remove i.G.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -28,7 +28,7 @@ const HomePage = () => {
 
                 <div className="w-full lg:max-w-4xl bg-[#011627] text-white p-6 md:p-10 rounded-lg shadow-lg bg-opacity-50 text-center">
                     <h1 className="text-3xl sm:text-4xl md:text-5xl text-[#41EAD4] mb-6">
-                        Ciphercurve GmbH i.G.
+                        Ciphercurve GmbH
                     </h1>
                     <a href="mailto:contact@ciphercurve.com" className="inline-block mt-6 px-6 py-3 bg-[#41EAD4] text-[#011627] font-semibold rounded-lg shadow-md hover:bg-[#1985A1] transition duration-300">
                         Contact Us
